fbf9407fa04ec22ba89a3a4d5d9e33da65d2eb8e
kemellpinto13 <161838079+kemellpinto13@users.noreply.github.com>
2024-12-06 11:10:16 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'geografia',
+    'qual é o menoor país do mundo?',
+    'vaticano'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da russia?',
+    'moscou'
+)
+
+criaCartao(
+    'matematica',
+    'O que é uma função?',
+    'Uma função é uma relação de um conjunto com um conjunto.',
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
